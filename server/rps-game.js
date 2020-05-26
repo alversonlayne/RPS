78197908458962de936abc7e64de46af840a8907
Alverson Layne <alverson.layne2@gmail.com>
2020-05-26 10:25:56 -0400

    editing server functionality

--- server/rps-game.js
+++ server/rps-game.js
@@ -3,19 +3,29 @@ class RpsGame {
     this._players = [p1, p2];
     this._turns = [null, null];
 
-    this._sendToPlayers('Rock Paper Scissors Starts!');
+    this._sendToPlayers("Rock Paper Scissors Starts!");
+
+    this._players.forEach((player, idx) => {
+      player.on("turn", (turn) => {
+        this._onTurn(idx, turn);
+      });
+    });
+  }
+
+  _sendToPlayer(playerIndex, msg) {
+    this._players[playerIndex].emit("message", msg);
   }
 
   _sendToPlayers(msg) {
     this._players.forEach((player) => {
-      player.emit('message', msg);
+      player.emit("message", msg);
     });
   }
 
   _onTurn(playerIndex, turn) {
     this._turns[playerIndex] = turn;
+    this._sendToPlayer(playerIndex, `You selected ${turn}`);
   }
-
 }
 
 module.exports = RpsGame;
